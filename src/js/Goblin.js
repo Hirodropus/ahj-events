5373cf5dd49a5b12bfa0561c6a5faba5925c8619
Hirodropus <hirodropus@gmail.com>
2026-06-29 23:17:22 +0500

    Внёс исправления

--- src/js/Goblin.js
+++ src/js/Goblin.js
@@ -7,13 +7,13 @@ export default class Goblin {
   }
 
   place(cell) {
-    // append автоматически переносит элемент из старой ячейки
+
     cell.append(this.element);
     this.currentCell = cell;
   }
 
   remove() {
-    // удаляем только гоблина, а не ячейку
+
     if (this.element.parentNode) {
       this.element.remove();
       this.currentCell = null;
